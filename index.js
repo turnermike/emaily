80f2ea95055b9b4bdcba6d32b6954b70b800d915
Mike <turner.mike@gmail.com>
2018-10-25 18:52:56 -0400

    added new client id and secret

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');     // using "common js modules"
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-// client id: 508753040406-jb4k1tra31mb17kmfkr93k94olm9ogl2.apps.googleusercontent.com
-// client secret: 5dcxk-LjqPzYmzUAxGfuKEbz
+// import express from 'express'           // example using ES2015 modules (node does not support)
 
-// import express from 'express'           // using ES2015 modules (node does not support)
+// create app object
+const app = express();
 
-const app = express();                  // create app object
+// register the strategy with passport
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log('accessToken', accessToken);
+    })
+);
 
-passport.use(new GoogleStrategy());     // register the strategy with passport
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 
+// https://accounts.google.com/o/oauth2/v2/auth?
+// response_type=code&
+// redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
+// scope=profile%20email&
+// client_id=508753040406-jb4k1tra31mb17kmfkr93k94olm9ogl2.apps.googleusercontent.com
 
 // route handler for get requests to '/'
 // req = request

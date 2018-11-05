5da487e78d176f18360dfd43acc80f90cb2f1518
Mike <turner.mike@gmail.com>
2018-11-05 17:59:46 -0500

    next step: 43

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,9 +13,17 @@ module.exports = app => {
     // route: /auth/google/callback
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    // logout user
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
     // get current user
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     });
+
+
 
 };

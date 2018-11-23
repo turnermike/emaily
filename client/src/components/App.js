df0ffc0bfae9f0c9bf029ac5b17d5ed06fc5962e
Mike <turner.mike@gmail.com>
2018-11-23 17:30:10 -0500

    started adding additional routes, current step: 68 3 min mark

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,22 @@
 // holds react router stuff
 
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';    // BrowserRouter and Router are both React Components
 
-const Header = () => <h2>Header</h2>
-const DAshboard = () => <h2>DAshboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
     return (
         <div>
-        Hi There!
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={Landing} />
+                    <Route path="/surveys" component={Dashboard} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };

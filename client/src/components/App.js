25dc4afa6be62deb79d94a53ebf3ffb145143ace
Mike <turner.mike@gmail.com>
2018-11-27 08:31:57 -0500

    current step: 70 2 min mark

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';    // BrowserRouter and Router are both React Components
 
-const Header = () => <h2>Header</h2>;
+// const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -13,8 +14,10 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={Landing} />
-                    <Route path="/surveys" component={Dashboard} />
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
                 </div>
             </BrowserRouter>
         </div>

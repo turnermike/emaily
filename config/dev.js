98677d8c96a8fc1305a6cdbfbfd4ab5846bf89e5
Mike <turner.mike@gmail.com>
2019-01-16 10:02:35 -0500

    commented config/.env from gitignore

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+// dev.js - don't commit to git
+module.exports = {
+    googleClientID: '1064421621364-dse17dj8vpe2icblg02cekqud4e8ej1j.apps.googleusercontent.com',
+    googleClientSecret: 'wlIQ0mva_LjLhkgvZR8l8dCL',
+    mongoURI: 'mongodb://mike:password1@ds147233.mlab.com:47233/emaily-dev',
+    cookieKey: 'lkjasdflkj2dkadf93jf093m12owkdm4',
+    stripePublishableKey: 'pk_test_7MuQkaCuIGOmbfnCFXB6izb7',
+    stripeSecretKey: 'sk_test_P3w35zYn2DgtM798Eyibdoq0',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:3000'
+}

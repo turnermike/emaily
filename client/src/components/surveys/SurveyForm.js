bc7e55f984d8a958462ecbd3559a24baf630802d
Mike <turner.mike@gmail.com>
2019-01-18 14:08:20 -0500

    completed 148 - redux form in practice

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -8,16 +8,24 @@ import React, { Component } from 'react';
 // Field helper for rendering any type of html form element (input, textarea, select, etc)
 import { reduxForm, Field } from 'redux-form';
 
+
+
+// this.props.handleSubmit is provided by reduxForm helper
+// arrow function is called automatically on submit
+// values object stores all the form data
 class SurveyForm extends Component {
 
   render() {
     return (
       <div>
-        <Field
-          type="text"
-          name="surveyTitle"
-          component="input"
-        />
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field
+            type="text"
+            name="surveyTitle"
+            component="input"
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }

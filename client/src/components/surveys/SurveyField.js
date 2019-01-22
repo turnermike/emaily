fc8f43730b85f1963d871436148746e2ea43934d
Mike <turner.mike@gmail.com>
2019-01-22 08:21:45 -0500

    completed Custom Field Components 077

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,17 @@
+// src/componenets/surveys/SurveyField.js
+
+// contains logic to render a single label and text input
+
+import React from 'react';
+// reduxForm helper allows redux-form to communicate with redux store
+// Field helper for rendering any type of html form element (input, textarea, select, etc)
+// import { reduxForm, Field } from 'redux-form';
+
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -7,7 +7,7 @@ import React, { Component } from 'react';
 // reduxForm helper allows redux-form to communicate with redux store
 // Field helper for rendering any type of html form element (input, textarea, select, etc)
 import { reduxForm, Field } from 'redux-form';
-
+import SurveyField from './SurveyField';
 
 
 // this.props.handleSubmit is provided by reduxForm helper
@@ -15,15 +15,19 @@ import { reduxForm, Field } from 'redux-form';
 // values object stores all the form data
 class SurveyForm extends Component {
 
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field
-            type="text"
-            name="surveyTitle"
-            component="input"
-          />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>

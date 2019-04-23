9a593ad9c554196f85ce19b095c4ee9613cd508d
Mike <turner.mike@gmail.com>
2019-04-23 13:58:23 -0400

    cleanup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -24,18 +24,19 @@ module.exports = app => {
 
   app.post('/api/surveys/webhooks', (req, res) => {
 
+    // use this when testing with Sendgrid
     // log the entire event when testing via Sendgrid "Test Your Integration" button
     // not all events have a email or url property, which will result in an error
     // const events = _.map(req.body, (event) => { // iterate the events object
     //   console.log(event);
     // });
 
-    const p = new Path('/api/surveys/:surveyId/:choice');                 // create parser object of url paths
+    const p = new Path('/api/surveys/:surveyId/:choice');                         // create parser object of url paths
 
-    // const events = _.map(req.body, ({ email, url }) => {                    // destructuring the 'event' object, we only need url/email
+    // const events = _.map(req.body, ({ email, url }) => {                       // destructuring the 'event' object, we only need url/email
 
-    //     // const pathname = new URL(url).pathname;                               // create path helper outside of map function
-    //     // const match = p.test(pathname);                                       // test the URL based on Path
+    //     // const pathname = new URL(url).pathname;                             // create path helper outside of map function
+    //     // const match = p.test(pathname);                                     // test the URL based on Path
     //   const match = p.test(new URL(url).pathname);                             // refactored the previous two lines
 
     //   if(match){
@@ -44,34 +45,33 @@ module.exports = app => {
 
     // });
 
-    // const compactEvents = _.compact(events);                                // lodash compact() helper will remove undefined elements
-    // const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');      // lodash uniqueBy() helper will remove any duplicates with same email and surveyId
+    // const compactEvents = _.compact(events);                                   // lodash compact() helper will remove undefined elements
+    // const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');         // lodash uniqueBy() helper will remove any duplicates with same email and surveyId
 
     // refactord map function using lodash .chain helper
-
     const events = _.chain(req.body)
 
-      .map(({ email, url }) => {                    // destructuring the 'event' object, we only need url/email
+      .map(({ email, url }) => {                                                  // destructuring the 'event' object, we only need url/email
 
-          // const pathname = new URL(url).pathname;                               // create path helper outside of map function
-          // const match = p.test(pathname);                                       // test the URL based on Path
+        // const pathname = new URL(url).pathname;                               // create path helper outside of map function
+        // const match = p.test(pathname);                                       // test the URL based on Path
         const match = p.test(new URL(url).pathname);                             // refactored the previous two lines
 
         if(match){
-          return { email, surveyId: match.surveyId, choice: match.choice }
+          return { email, surveyId: match.surveyId, choice: match.choice }       // return results
         }
 
       })
 
-      .compact()                                // lodash compact() helper will remove undefined elements
-      .uniqBy('email', 'surveyId')              // lodash uniqueBy() helper will remove any duplicates with same email and surveyId
-      .value();                                 // lodash value() pull the underlining/remaining array
+      .compact()                                                                  // lodash compact() helper will remove undefined elements
+      .uniqBy('email', 'surveyId')                                                // lodash uniqueBy() helper will remove any duplicates with same email and surveyId
+      .value();                                                                   // lodash value() pull the underlining/remaining array
 
     console.log(new Date().toLocaleString() + ' -- unique event ----------------------');
     console.log(events);
     console.log('------------------------');
 
-    res.send({});                                                           // send a webhook response to sendgrid, or the webhook requests will continue
+    res.send({});                                                                 // send a webhook response to sendgrid, or the webhook requests will continue
 
 
 

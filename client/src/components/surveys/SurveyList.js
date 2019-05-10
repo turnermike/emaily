2268201d5d72f61231058ffcb15e28ca56cc7500
Mike <turner.mike@gmail.com>
2019-05-10 17:54:10 -0400

    completed 189 - writting react to redux with expected results

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions';
 
+
 class SurveyList extends Component {
 
   componentDidMount() {
